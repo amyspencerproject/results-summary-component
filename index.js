8d4241fb1ceeb29ce9de852b7a0bc30917aaf757
amyspencerproject <amy@spencerproject.com>
2023-08-01 13:18:18 -0600

    First try setting up node js to pull data.json

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const ratingInput = fs.readFileSync("./input.txt", "utf-8");
+
+console.log(ratingInput);
+
+// proving that the index.js is running in the browser
+// console.log("Something worked! 🚀");
---
+++ scratch.js
@@ -0,0 +1,9 @@
+// import data from "./data.json";
+// console.log(data);
+
+// const getRating = async function () {
+//   const data = await fetch("./data.json");
+//   console.log(data);
+// };
+
+// getRating();
