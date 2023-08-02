b0c4c41b9bafd3d37be43eeca3167d1c02c8cce8
amyspencerproject <amy@spencerproject.com>
2023-08-01 19:19:37 -0600

    Read and parse data.json file. Set up server and routing

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-const ratingInput = fs.readFileSync("./input.txt", "utf-8");
+// FILES
+const ratingInput = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
+const ratingObject = JSON.parse(ratingInput);
 
-console.log(ratingInput);
+// console.log(ratingObject);
 
-// proving that the index.js is running in the browser
-// console.log("Something worked! 🚀");
+// SERVER
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/summary") {
+    res.end("this is the results summary");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end(<h1>"Page not found!"</h1>);
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
